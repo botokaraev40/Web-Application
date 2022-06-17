984a4126a36f204e40c03ea9bf23a879acdbcb41
Kadyr <botokaraevkadyr@gmail.com>
2022-06-17 08:26:01 +0300

    Commit_lab_2_3

--- lab2/app/app.py
+++ lab2/app/app.py
@@ -1,3 +1,5 @@
+import sys
+
 from flask import Flask, render_template, request, url_for, make_response
 
 app = Flask(__name__)
@@ -27,9 +29,37 @@ def cookies():
 
 @app.route('/form', methods=['GET', 'POST'])
 def form():
-    return render_template('form.html')
+    return render_template('login.html')
 
 @app.route('/check', methods=['GET', 'POST'])
 def check():
+    phone_number = ''
+    phone_number = request.form.get('phone-number')
+    counter = 0
+    symbol = 0
+    format_number = ''
 
-    return render_template('check.html')
+    if phone_number is None:
+        print("Value is None")
+    else:
+        for i in range(0, len(phone_number)):
+            if phone_number[i] == '0' or phone_number[i] == '1' or phone_number[i] == '2' or phone_number[i] == '3' or \
+                    phone_number[i] == '4' or phone_number[i] == '5' or phone_number[i] == '6' or phone_number[
+                i] == '7' or \
+                    phone_number[i] == '8' or phone_number[i] == '9':
+                counter += 1
+                format_number += phone_number[i]
+            elif phone_number[i] != ' ' and phone_number[i] != '(' and phone_number[i] != '(' and phone_number[
+                i] != ')' and \
+                    phone_number[i] != '-' and phone_number[i] != '.' and phone_number[i] != '+':
+                symbol += 1
+    if counter == 11 and phone_number[0] == '+' and phone_number[1] == '7':
+        format_number = '8' + format_number[1:len(format_number)]
+
+    elif counter == 11 and phone_number[0] == '8':
+        format_number = format_number
+
+    elif counter == 10:
+        format_number = '8' + format_number
+
+    return render_template('check.html', symbol=symbol, counter=counter, format_number=format_number, phone_number=phone_number)
--- lab3/app/app.py
+++ lab3/app/app.py
@@ -2,14 +2,19 @@ from flask import Flask, render_template, session, request, redirect, url_for, f
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
 
 login_manager = LoginManager()
-login_manager.login_view = 'login'
-login_manager.login_message = 'Для доступа к данной странице необходимо пройти процедуру аутентификации.'
-login_manager.login_message_category= 'warning'
 
 app = Flask(__name__)
 application = app
 
 login_manager.init_app(app)
+login_manager.login_view = 'login'
+login_manager.login_message = 'Для доступа к данной странице необходимо аутентифицироваться.'
+login_manager.login_message_category = 'warning'
+
+app.config.from_pyfile('config.py')
+
+def get_users():
+    return [{'user_id': '1', 'login': 'user', 'password': 'qwerty'}]
 
 class User(UserMixin):
     def __init__(self, user_id, login, password):
@@ -26,42 +31,46 @@ def load_user(user_id):
             return User(**user)
     return None
 
-app.config.from_pyfile('config.py')
-def get_users():
-    return [{'user_id': '1', 'login': 'user', 'password:' 'qwerty'}]
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/visits')
 def visits():
-    if session.get('visits_count') is None:
-        session['visits_count'] = 1
+    if session.get('visits'):
+        session['visits'] += 1
     else:
-        session['visits_count'] += 1
+        session['visits'] = 1
+
     return render_template('visits.html')
-@app.route('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        login_ = request.form.get('login')
+        login = request.form.get('login')
         password = request.form.get('password')
-        remember_me = request.form.get('remember me')
-        for user in get_users():
-            if user['login'] == login_ and user['password'] == password:
-                login_user(User(**user), remember=remember_me)
-                flash("Вы успешно прошли процедуру аутентификации.", 'success')
-                next_ = request.args.get('next')
-                return redirect(next_ or url_for('index'))
-            flash("Введены неверные логин и/или пароль.", "danger")
-    return render_template('login.html')
+        remember_me = request.form.get('remember_me') == 'on'
+        if login and password:
+            for user in get_users():
+                if user['login'] == login and user['password'] == password:
+                    user_object = User(**user)
+                    login_user(user_object, remember=remember_me)
+                    flash('Вы успешно аутентифицированы.', 'success')
+
+                    next = request.args.get('next')
+
+                    return redirect(next or url_for('index'))
+        flash('Вы неуспешно аутентифицированы.', 'danger')
+    return  render_template('login.html')
 
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
-
+    return  redirect(url_for('index'))
 
 @app.route('/secret_page')
+@login_required
 def secret_page():
-    return render_template(url_for('secret_page.html'))
-
+    return render_template('secret_page.html')
